985f80ef90a2df853dba22868a748475d6283cb6
Rahul singh <rs618642@gmail.com>
2023-04-05 16:39:06 +0530

    refactor file position

--- src/test/java/org/testclasses/VerifyUpcomingDropdown.java
+++ src/test/java/org/testclasses/VerifyUpcomingDropdown.java
@@ -3,7 +3,6 @@ package org.testclasses;
 import PageClasses.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
