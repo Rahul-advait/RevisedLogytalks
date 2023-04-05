e35ca498f28cf266bbdfe61c83fdcc51acddda9f
Rahul singh <rs618642@gmail.com>
2023-04-05 19:18:48 +0530

    made some changes on window maximze

---
+++ src/main/java/PageClasses/UpcomingConferenceResultPage.java
@@ -0,0 +1,31 @@
+package PageClasses;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class UpcomingConferenceResultPage {
+    private WebDriver driver;
+    private String URL = "?category=7";
+    private String COURSES_LIST = "//div[#'nav-home']/?/?/div[@safeclass~'\\brow\\b']/div";
+
+    public boolean isOpen() {
+        return driver.getCurrentUrl().contains(URL);
+    }
+
+    public int coursesCount() {
+        List<WebElement> conferenceList = driver.findElements(By.xpath(COURSES_LIST));
+        return conferenceList.size();
+    }
+
+    public boolean verifySearchResult() {
+        boolean result = false;
+        if (coursesCount() > 0) {
+            result = true;
+        }
+        result = isOpen() && result;
+        return result;
+    }
+}
--- src/test/java/org/testclasses/VerifyUpcomingDropdown.java
+++ src/test/java/org/testclasses/VerifyUpcomingDropdown.java
@@ -2,20 +2,25 @@ package org.testclasses;
 
 import PageClasses.LoginPage;
 import PageClasses.UpcomingConference;
+import PageClasses.UpcomingConferenceResultPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.testng.Assert.*;
+
 public class VerifyUpcomingDropdown {
-    WebDriver driver;
-    String baseURL;
+    private WebDriver driver;
+    private String baseURL;
     @BeforeClass
     public void setUp() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         baseURL = "https://logytalks.com/";
         driver.get(baseURL);
@@ -26,10 +31,13 @@ public class VerifyUpcomingDropdown {
         LoginPage login = new LoginPage(driver);
         login.open();
         login.signInWith("rahulsingh@yopmail.com", "test@123");
+
         UpcomingConference upcomingConferencePage = new UpcomingConference(driver);
-//        Assert.assertTrue(upcomingConferencePage.isOpen());
-        upcomingConferencePage.search("Engineering");
-//        boolean searchResult = verifySearchResult();
+        upcomingConferencePage.search("Business");
+
+        UpcomingConferenceResultPage resultPage = new UpcomingConferenceResultPage();
+        boolean searchResult = resultPage.isOpen();
+        assertTrue(searchResult);
     }
     @AfterClass
     public void tearDown() {
